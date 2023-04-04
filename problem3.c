33efff88b0e6df8dd2dbc3e6b90d4a56e578fd51
Ishtiak01-ru <mdishtiakahmed07@gmail.com>
2023-04-04 13:11:34 +0600

    problem.c

--- problem3.c
+++ problem3.c
@@ -4,11 +4,15 @@ int main()
     int n;
 
     scanf("%d",&n);
-    if(n%2==0){
+    if(n>=0 && n%2==0){
         printf("The number is Even");
     }
-     else{
+     else if(n >=0 && n%2 != 0)
+     {
         printf("Odd");
+     }
+     else{
+        printf("Number is Negative");
      }
      return 0;
 
